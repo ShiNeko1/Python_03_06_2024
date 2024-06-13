c7f540dd0c36214ee22a0cb7b779115bd99eb057
Anastasiia Solokha <shi.neko@outlook.com>
2024-06-13 20:06:25 +0300

    Third commit: number input check was added to HW 3.1

--- HW 3.1.py
+++ HW 3.1.py
@@ -3,13 +3,30 @@ from decimal import Decimal
 math_operators = ('+', '-', '*', '/', 'add', 'sub', 'mul', 'div')
 
 first_number = input("Input first number: ")
+
+while True:
+    try:
+        float(first_number)
+        break
+    except ValueError:
+        print("INVALID number! Letters and symbols (except a dot and -) are not accepted.")
+        first_number = input("Input first number: ")
+
 second_number = input("Input second number: ")
+
+while True:
+    try:
+        float(second_number)
+        break
+    except ValueError:
+        print("INVALID number! Letters and symbols (except a dot and -) are not accepted.")
+        second_number = input("Input second number: ")
+
 input_math_operator = input("Input operator (+, -, *, /, add, sub, mul, div): ")
 
-
-if input_math_operator not in math_operators:
-    print("Invalid math operator was entered")
-    exit()
+while input_math_operator not in math_operators:
+    print("INVALID math operator! Choose from +, -, *, /, add, sub, mul, div.")
+    input_math_operator = input("Input math operator: ")
 
 first_number = Decimal(first_number)
 second_number = Decimal(second_number)
@@ -26,6 +43,5 @@ elif input_math_operator in math_operators[2::4]:
 else:
     result_number = first_number / second_number
     result_output_str = f"The result of {first_number} dividing {second_number} equals {result_number}"
-
 
 print(result_output_str)
