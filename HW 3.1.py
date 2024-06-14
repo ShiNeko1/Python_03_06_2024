a6208258736bb4d04b64238c51c55d0c371b241e
Anastasiia Solokha <shi.neko@outlook.com>
2024-06-14 09:58:02 +0300

    Fifth commit: devision by 0 check was added to HW 3.1

--- HW 3.1.py
+++ HW 3.1.py
@@ -1,4 +1,5 @@
 from decimal import Decimal
+from time import sleep
 
 math_operators = ('+', '-', '*', '/', 'add', 'sub', 'mul', 'div')
 
@@ -39,9 +40,27 @@ elif input_math_operator in math_operators[1::4]:
     result_output_str = f"The result of {first_number} subtracting {second_number} equals {result_number}"
 elif input_math_operator in math_operators[2::4]:
     result_number = first_number * second_number
-    result_output_str = f"The result of {first_number} multiplying {second_number} equals {result_number}"
+    result_output_str = f"The result of {first_number} multiplying by {second_number} equals {result_number}"
 else:
-    result_number = first_number / second_number
-    result_output_str = f"The result of {first_number} dividing {second_number} equals {result_number}"
+    if second_number == 0:
+        print("UNAUTHORIZED action detected! Division by 0 is not allowed! We are calling the police. >:(")
+        sleep(4)
+        print("Hey!")
+        sleep(1)
+        print("DO NOT even try to run to the INFINITY!")
+        sleep(2)
+        print("We")
+        sleep(0.5)
+        print("will")
+        sleep(0.5)
+        print("get you there")
+        sleep(0.5)
+        print("TOO!")
+        sleep(1)
+        print("Guys, catch him!")
+        exit()
+    else:
+        result_number = first_number / second_number
+        result_output_str = f"The result of {first_number} dividing by {second_number} equals {result_number}"
 
 print(result_output_str)
