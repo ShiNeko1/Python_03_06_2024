2c3526d5ff79ee09011ea46a34af65feb44a88bc
Anastasiia Solokha <shi.neko@outlook.com>
2024-07-21 17:49:22 +0300

    Second commit for L12: I just wanted to use a generator in HW 12.1

--- HW 12.1.py
+++ HW 12.1.py
@@ -1,26 +1,40 @@
 import codecs
 
 
-def text_cleaner(text, symb1, symb2):
+def gen_remove_elm(text: str, symb1: str, symb2: str) -> list:
+    """This generator finds all substrings in the text which are between two symbols"""
+
     begin = text.find(symb1)
-    end = text.find(symb2)
+    end = text.find(symb2, begin+1)
 
     while begin != -1:
-        text = text.replace(text[begin:end + 1], "")
-        begin += text[begin:].find(symb1)
-        end = text[begin:].find(symb2) + begin
+        yield text[begin:end+1]
+        begin = text.find(symb1, end+1)
+        end = text.find(symb2, begin+1)
 
+    return
+
+
+def text_cleaner(text: str, to_remove: list) -> str:
+    """This function removes all the substrings form the list in a text"""
+
+    for elm in to_remove:
+        text = text.replace(elm, "")
     return text
 
 
-def delete_empty_lines(text):
-    return "\n".join([line.strip() for line in text.splitlines() if line.strip()])
+def delete_empty_lines(text: str) -> str:
+    """Removes all empty lines in a text"""
+    return "\n".join(line.strip() for line in text.splitlines() if line.strip())
 
 
 def delete_html_tags(html_file, result_file='cleaned.txt'):
+    """This function deletes tags in a text and writes the cleared text into a new file"""
+
     with codecs.open(html_file, 'r', 'utf-8') as file:
         html = file.read()
-        cleaned_text = text_cleaner(html, "<", ">")
+
+        cleaned_text = text_cleaner(html, list(gen_remove_elm(html, "<", ">")))
         cleaned_text = delete_empty_lines(cleaned_text)
 
         with codecs.open(result_file, 'w', 'utf-8') as result:
