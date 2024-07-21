05c11dc19cf9aff0446e0fe1dfc16d9044a4a5ea
Anastasiia Solokha <shi.neko@outlook.com>
2024-07-21 18:48:34 +0300

    Third commit: HW 12.2 was added

---
+++ HW 12.2.py
@@ -0,0 +1,87 @@
+class Item:
+
+    def __init__(self, name, price, description, dimensions):
+        self.price = price
+        self.description = description
+        self.dimensions = dimensions
+        self.name = name
+
+    def __str__(self):
+        return f"{self.name}, price: {self.price}"
+
+
+class User:
+
+    def __init__(self, name, surname, numberphone):
+        self.name = name
+        self.surname = surname
+        self.numberphone = numberphone
+
+    def __str__(self):
+        return f"{self.name} {self.surname}"
+
+
+class Purchase:
+    def __init__(self, user):
+        self.products = {}
+        self.user = user
+        self.total = 0
+
+    def add_item(self, item, cnt):
+        self.products[item] = cnt
+
+    def __str__(self):
+        """ Метод виведення інформації про кошик """
+        items_in_cart = ""
+        for item, cnt in self.products.items():
+            items_in_cart += f"{str(item.name)}: {cnt} pcs. \n"
+        return f"User: {self.user}\nItems:\n{items_in_cart}"
+
+    def get_total(self):
+        """ Метод отримання загальної вартості товару у кошику """
+        total = 0
+        for key, cnt in self.products.items():
+            total += key.price * cnt
+        return total
+
+
+
+lemon = Item('lemon', 5, "yellow", "small", )
+apple = Item('apple', 2, "red", "middle", )
+pineapple = Item('pineapple', 10, "elite", "big", )
+kiwi = Item('pineapple', 7, "green and hairy", "small", )
+print(lemon)  # lemon, price: 5
+
+buyer = User("Ivan", "Ivanov", "02628162")
+buyer2 = User("Jack", "Jackson", "02644456")
+print(buyer)  # Ivan Ivanov
+
+cart = Purchase(buyer)
+cart.add_item(lemon, 4)
+cart.add_item(apple, 20)
+print(cart)
+"""
+User: Ivan Ivanov
+Items:
+lemon: 4 pcs.
+apple: 20 pcs.
+"""
+assert isinstance(cart.user, User) is True, 'Екземпляр класу User'
+assert cart.get_total() == 60, "Всього 60"
+assert cart.get_total() == 60, 'Повинно залишатися 60!'
+cart.add_item(apple, 10)
+print(cart)
+"""
+User: Ivan Ivanov
+Items:
+lemon: 4 pcs.
+apple: 10 pcs.
+"""
+
+assert cart.get_total() == 40, "Всього 40"
+
+cart2 = Purchase(buyer2)
+cart2.add_item(kiwi, 4)
+cart2.add_item(lemon, 2)
+cart2.add_item(pineapple, 1)
+print(cart2)
